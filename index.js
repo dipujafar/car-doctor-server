4620ef796f71d23c92bb111d4c2003b880510bd2
dipujafar <131136004+dipujafar@users.noreply.github.com>
2023-11-01 00:51:07 +0600

    try to authorization with jwt

--- index.js
+++ index.js
@@ -1,15 +1,56 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const cookiesParser = require("cookie-parser");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookiesParser());
 
+// custom middleware
+const logger = async (req, res, next) => {
+  console.log("call", req.host, req.originalUrl);
+  next();
+};
+const verifyToken = async (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized Access" });
+    }
+    req.user = decoded;
+    next();
+  });
+  console.log("token", token);
+  next();
+};
+
+// const verifyToken = async (req, res, next) => {
+//   const token = req.cookies?.token;
+//   if (!token) {
+//     return res.status(401).send({ message: "Unauthorized" });
+//   }
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({ message: "Unauthorized" });
+//     }
+//     req.user = decoded;
+//     next();
+//   });
+// };
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@jafardipu.hwlq4pv.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,13 +70,52 @@ async function run() {
     const serviceCollection = client.db("carsDotor").collection("services");
     const bookingCollection = client.db("carsDotor").collection("booking");
 
-    //auth related apis
+    // auth related apis
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       console.log(user);
-      const token = jwt.sign(user, "secret", { expiresIn: "1h" });
-      res.send(token);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
     });
+
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("LogOut User", user);
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    });
+
+    //auth related apis
+    // app.post("/jwt", async (req, res) => {
+    //   const user = req.body;
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    //     expiresIn: "1h",
+    //   });
+    //   res
+    //     .cookie("token", token, {
+    //       httpOnly: true,
+    //       secure: false,
+    //     })
+    //     .send({ Success: true });
+
+    //   // const user = req.body;
+    //   // const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    //   //   expiresIn: "1h",
+    //   // });
+    //   // res
+    //   //   .cookie("token", token, {
+    //   //     httpOnly: true,
+    //   //     secure: false,
+    //   //   })
+    //   //   .send({ Success: true });
+    // });
 
     // service related apis
 
@@ -56,8 +136,9 @@ async function run() {
     });
 
     //booking
-    app.get("/book", async (req, res) => {
+    app.get("/book", verifyToken, async (req, res) => {
       console.log(req.query.email);
+
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
